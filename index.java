dec00ff0591a86b11fdcae8ac3d6a91c1e99e445
thiagoparavati2 <thiagoparavati9@gmail.com>
2022-11-07 22:15:31 -0300

    Teste commit 2

--- index.java
+++ index.java
@@ -3,5 +3,6 @@ public class index {
     public static void main(String[] args) {
         System.out.println("OLA MUNDO");
         System.out.println("Teste commit");
+        System.out.println("Teste commit 2");
     }
 }
